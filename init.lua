c578ad4253d2144b643f623ab387518213965d79
Davide Pasca <dpasca@gmail.com>
2024-10-24 18:36:28 +0900

    Added Avante plugin

--- init.lua
+++ init.lua
@@ -275,8 +275,61 @@ require("lazy").setup({
   { "editorconfig/editorconfig-vim" },
 
   -- AI assistance
+  -- NOTE: Avante wants to include .lua version, but that
+  --  doesn't work for us in Windows (chat ok, but not autocomplete)
   { "github/copilot.vim" },
-  { "TabbyML/vim-tabby" },
+
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      provider = "claude",
+      auto_suggestions_provider = "claude",
+      -- NOTE: copilot.lua seems broken in Windows
+      --provider = "copilot",
+      --auto_suggestions_provider = "copilot",
+    },
+
+    build = vim.fn.has("win32") == 1
+        and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      --"zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
 
   -- TMux integration
   { "christoomey/vim-tmux-navigator" },
